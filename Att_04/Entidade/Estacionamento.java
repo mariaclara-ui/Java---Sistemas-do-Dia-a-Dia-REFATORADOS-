a6dbe9cd2f38e17450d6462f10941a193290a857
mariaclara-ui <Mariaclaraduartedeniz@gmail.com>
2026-05-22 20:11:37 -0300

    Add files via upload

---
+++ Att_04/Entidade/Estacionamento.java
@@ -0,0 +1,19 @@
+package Java_criacao_app.Atividade_Sistemas_dia_a_dia.Att_04.Entidade;
+
+public class Estacionamento {
+    private String placa;
+    private String tipoVeiculo;
+    public String getPlaca() {
+        return placa;
+    }
+    public String getTipoVeiculo() {
+        return tipoVeiculo;
+    }
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+    public void setTipoVeiculo(String tipoVeiculo) {
+        this.tipoVeiculo = tipoVeiculo;
+    }
+}
+
